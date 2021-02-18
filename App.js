8ef7409b9aa9de99323387ff8b29db65e9186d42
Bob Rusch <git-commit-address>
2021-02-18 15:46:10 -0500

    added eslint & prettier

--- App.js
+++ App.js
@@ -1,28 +1,25 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform,
-} from "react-native";
+import { StatusBar, StyleSheet, SafeAreaView } from "react-native";
 
 import { ResturantList } from "./src/components/ResturantList/ResturantList";
+
+StatusBar.currentHeight === null ? 0 : StatusBar.currentHeight;
 
 export default function App() {
   return (
     <>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          marginTop: !StatusBar.currentHeight ? 0 : StatusBar.currentHeight,
-        }}>
+      <SafeAreaView style={styles.siteContainer}>
         <ResturantList />
       </SafeAreaView>
+      <ExpoStatusBar style="light" />
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  siteContainer: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight === null ? 0 : StatusBar.currentHeight,
+  },
+});
--- src/components/ResturantList/ResturantList.js
+++ src/components/ResturantList/ResturantList.js
@@ -1,16 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export const ResturantList = () => {
   return (
     <>
-      <View style={{ padding: 10, backgroundColor: "green" }}>
+      <View style={styles.searchContainer}>
         <Text>Reaturant List</Text>
       </View>
-      <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+      <View style={styles.listContainer}>
         <Text>lixt</Text>
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  searchContainer: { padding: 10, backgroundColor: "green" },
+  listContainer: { flex: 1, padding: 16, backgroundColor: "blue" },
+});
