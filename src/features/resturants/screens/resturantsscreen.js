8f8a1b39dab6e89cbc865dd838846c923c373bc3
Bob Rusch <git-commit-address>
2021-02-20 11:57:10 -0500

    converted to Styled Components

---
+++ src/features/resturants/screens/resturantsscreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+const Testdata = {
+  name: "Golden Needle",
+  icon: "🍴",
+  photos: [
+    "https://www.kohinoor-joy.com/wp-content/uploads/2020/01/indo-chinese-food.jpg",
+  ],
+  address: "123 Any Street",
+  openingHours: "11:30 - 10:00",
+  rating: 4,
+  isClose: false,
+};
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard resturant={Testdata} />
+    </RestaurantListContainer>
+  </SafeArea>
+);
